3ec76e8af76a2e7f2e890a15820e1c9fbe5dbade
PraderioM <PraderioM@Gmail.com>
2018-06-10 18:09:27 +0200

    Small refactor.

--- factorization/primality_tests.py
+++ factorization/primality_tests.py
@@ -4,28 +4,6 @@ from random import randint
 import factorization.utils as utils
 
 
-def get_order(a, n):
-    """
-    Computes the order of the integer a modulo in Z/(n).
-    :param a: an integer.
-    :param n: an integer.
-    :return: order of a in Z/(n) or -1 if a has no inverse in Z/(n)
-    """
-    a = abs(a)
-    n = abs(n)
-    order = 1
-    prod = a % n
-    while prod != 1:
-        # if prod is 0 then a has no inverse in Z/(n) and we stop the algorithm.
-        if prod == 0:
-            return -1
-        # compute always the power modulo n in order to avoid overflow.
-        prod = (prod * a) % n
-        order += 1
-
-    return 1
-
-
 def is_decomposed(factorization,
                   primality_tests=None):
     """
@@ -47,39 +25,6 @@ def is_decomposed(factorization,
             return False
     # If all primality test were passed we can assume the factorization object is factorized.
     return True
-
-
-def get_jacobi_symbol(a, n):
-    """
-    :param a: a positive integer (we don't know how to compute fast the Jacobi symbol for negative integers).
-    :param n: a positive odd integer.
-    :return: the Jacobi symbol (a/n)
-    """
-    if a < 0:
-        raise ValueError('a should be non negative.')
-    if (n % 2) == 0:
-        raise ValueError('n should be odd but it is not.')
-
-    # get the residue of a modulo n.
-    a = a % n
-    # if a is zero then the Jacobi symbol is 0.
-    if a == 0:
-        return 0
-
-    # Extract all powers of 2 multiplying a and compute the Jacobi symbol for those.
-    prod = 1
-    ratio = 1 if (n - 1) % 8 in [0, 6] else -1  # (2/n)=(-1)^((n^2-1)/8)
-    while (a % 2) == 0:
-        a /= 2
-        prod *= ratio
-
-    # if it is a power of 2 then the Jacobi symbol is prod.
-    if a == 1:
-        return prod
-
-    # Now that a is odd we can apply the quadratic reciprocity.
-    prod = prod if ((a - 1) % 4) == 0 or ((n - 1) % 4) == 0 else -prod  # we combine it with the previous results.
-    return prod * get_jacobi_symbol(n, a)  # apply the algorithm recursively.
 
 
 def trial_division(n):
@@ -118,7 +63,7 @@ def is_pseudoprime_to_base_b(n, b=2):
     if utils.euclidean_algorithm_m_c_d(n, b) != 1:
         return False
     # get the order of b in Z/(n)
-    order = get_order(b, n)
+    order = utils.get_order(b, n)
     # return True if  the order divides n - 1 and else return False
     return ((n - 1) % order) == 0
 
@@ -164,7 +109,7 @@ def is_euler_pseudoprime_to_base_b(n, b=2):
             return False
 
     # get Jacobi symbol.
-    jb_symbol = get_jacobi_symbol(b, n)
+    jb_symbol = utils.get_jacobi_symbol(b, n)
     # get b^((n-1)/2) mod n.
     power_mod_n = utils.get_power_mod_n(b, (n-1)/2, n)
     # return True if the two previous numbers are equal modulo n else False.
--- factorization/utils.py
+++ factorization/utils.py
@@ -445,3 +445,58 @@ def get_element_from_vector(vector, factor_base, mod):
         prod = (prod * get_power_mod_n(factor, power, mod)) % mod
 
     return prod
+
+
+def get_order(a, n):
+    """
+    Computes the order of the integer a modulo in Z/(n).
+    :param a: an integer.
+    :param n: an integer.
+    :return: order of a in Z/(n) or -1 if a has no inverse in Z/(n)
+    """
+    a = abs(a)
+    n = abs(n)
+    order = 1
+    prod = a % n
+    while prod != 1:
+        # if prod is 0 then a has no inverse in Z/(n) and we stop the algorithm.
+        if prod == 0:
+            return -1
+        # compute always the power modulo n in order to avoid overflow.
+        prod = (prod * a) % n
+        order += 1
+
+    return 1
+
+
+def get_jacobi_symbol(a, n):
+    """
+    :param a: a positive integer (we don't know how to compute fast the Jacobi symbol for negative integers).
+    :param n: a positive odd integer.
+    :return: the Jacobi symbol (a/n)
+    """
+    if a < 0:
+        raise ValueError('a should be non negative.')
+    if (n % 2) == 0:
+        raise ValueError('n should be odd but it is not.')
+
+    # get the residue of a modulo n.
+    a = a % n
+    # if a is zero then the Jacobi symbol is 0.
+    if a == 0:
+        return 0
+
+    # Extract all powers of 2 multiplying a and compute the Jacobi symbol for those.
+    prod = 1
+    ratio = 1 if (n - 1) % 8 in [0, 6] else -1  # (2/n)=(-1)^((n^2-1)/8)
+    while (a % 2) == 0:
+        a /= 2
+        prod *= ratio
+
+    # if it is a power of 2 then the Jacobi symbol is prod.
+    if a == 1:
+        return prod
+
+    # Now that a is odd we can apply the quadratic reciprocity.
+    prod = prod if ((a - 1) % 4) == 0 or ((n - 1) % 4) == 0 else -prod  # we combine it with the previous results.
+    return prod * get_jacobi_symbol(n, a)  # apply the algorithm recursively.
